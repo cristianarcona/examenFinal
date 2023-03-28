19221c23468c68e4f06aca02f83e1b15866a47a7
cristianarcona <54252268+cristianarcona@users.noreply.github.com>
2023-03-27 20:25:41 -0400

    Tercer COmmit

--- productos.js
+++ productos.js
@@ -1,5 +1,7 @@
 const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
 button1.addEventListener('click', mostrarMensaje)
+button2.addEventListener('click', mostrarMensaje)
 
 function mostrarMensaje(){
     alert("Tenemos Descuentos")
